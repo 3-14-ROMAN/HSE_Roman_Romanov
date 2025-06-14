45e494e98b2b4b06330b6408177dc007124dff2d
Романов Роман <roman@192.168.0.18>
2025-06-14 12:20:11 +0700

    Обновление: суббота, 14 июня 2025 г. 12:20:11 (+07)

---
+++ lesson 1/lesson_1.py
@@ -0,0 +1,30 @@
+a = 1 # int
+b = 5.5 # float
+c = True # bool
+d = False # bool
+print(a+b+c)
+
+name = 'Roman'
+surmame = 'Romanov'
+print (name + ' ' + surmame)
+print (name [-4])
+
+company = 'АО "Gate 127"'
+print (company)
+
+company_list = ['АО "Gate 127"',
+                 'ООО "Б 152"',
+                 'ООО "Футбол"']
+print (company_list)
+print (company_list[0])
+
+
+company_tuple = ('АО "Gate 127"',
+                 'ООО "Б 152"',
+                 'ООО "Футбол"')
+print (company_tuple)
+print (company_tuple[-1])
+
+company_dict = {'АО "Gate 127"',
+                 'ООО "Б 152"',
+                 'ООО "Футбол"'}
