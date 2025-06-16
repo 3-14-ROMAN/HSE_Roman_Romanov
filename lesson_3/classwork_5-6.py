566dd494db7425fe8922344ea8c1a4c62a274031
Романов Роман <roman@192.168.0.14>
2025-06-16 17:10:44 +0700

    Обновление: понедельник, 16 июня 2025 г. 17:10:44 (+07)

--- lesson_3/classwork_5-6.py
+++ lesson_3/classwork_5-6.py
@@ -1,21 +1,39 @@
-import json
-
 print('Hello world')
 
+import csv
+import json
 
-abs_path = '/Users/roman/Desktop/HSE_Roman_Romanov/lesson_3/traders.txt'
-rel_path = 'lesson_3/traders.txt'
+abs_path_txt = '/Users/roman/Desktop/HSE_Roman_Romanov/lesson_3/traders.txt'
+rel_path_txt = 'lesson_3/traders.txt'
 
-file = open(rel_path, 'r')
-data = file.readlines()
-file.close()
-
-file = open('/Users/roman/Desktop/HSE_Roman_Romanov/lesson_3/1000_efrsb_messages.json', 'rb') as file
-data = json.load(file)
-file.close()
-
-with open('/Users/roman/Desktop/HSE_Roman_Romanov/lesson_3/traders.txt', 'r') as file:
+with open(rel_path_txt, 'r') as file:
     data = file.readlines()
+
+abs_path_json = '/Users/roman/Desktop/HSE_Roman_Romanov/lesson_3/1000_efrsb_messages.json'
+rel_path_json = '1000_efrsb_messages.json'
+
+with open(abs_path_json, 'r') as file:
+    data = json.load(file)
+
+
+abs_path_csv = '/Users/roman/Desktop/HSE_Roman_Romanov/lesson_3/traders.csv'
+rel_path_csv = 'traders.csv'
+
+with open(abs_path_csv, 'r') as file:
+    csv_file = csv.reader(file)
+    for row in csv_file:
+        print(', '.join(row))
+
+best_delimiter = | 
+
+
+
+# # with open('/Users/roman/Desktop/HSE_Roman_Romanov/lesson_3/traders.txt', 'r') as file:
+# #     data = file.readlines()
+
+# file = open(rel_path_txt, 'r')
+# data = file.readlines()
+# file.close()
 
 
 print('stop')
