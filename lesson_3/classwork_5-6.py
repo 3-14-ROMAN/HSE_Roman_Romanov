92e01c478af2850f8ebfbde4ad8d8af50b637d7e
Романов Роман <roman@192.168.0.14>
2025-06-16 16:17:58 +0700

    Обновление: понедельник, 16 июня 2025 г. 16:17:58 (+07)

--- lesson_3/classwork_5-6.py
+++ lesson_3/classwork_5-6.py
@@ -1,7 +1,17 @@
+import json
+
 print('Hello world')
 
-file = open('/Users/roman/Desktop/HSE_Roman_Romanov/lesson_3/traders.txt', 'r')
+
+abs_path = '/Users/roman/Desktop/HSE_Roman_Romanov/lesson_3/traders.txt'
+rel_path = 'lesson_3/traders.txt'
+
+file = open(rel_path, 'r')
 data = file.readlines()
+file.close()
+
+file = open('/Users/roman/Desktop/HSE_Roman_Romanov/lesson_3/1000_efrsb_messages.json', 'rb') as file
+data = json.load(file)
 file.close()
 
 with open('/Users/roman/Desktop/HSE_Roman_Romanov/lesson_3/traders.txt', 'r') as file:
